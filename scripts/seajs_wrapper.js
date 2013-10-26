fd882e2da6e899d5eedca18c1ce05a702eed5ccf
matin0728 <matin0728@gmail.com>
2013-10-26 13:44:28 +0800

    update seajs wrapper.

--- scripts/seajs_wrapper.js
+++ scripts/seajs_wrapper.js
@@ -6,56 +6,64 @@ define(function(require, exports) {
 	ZH.parentChildMap = null
 
   var registry = ZH.core.Registry.getInstance();
-	ZH.initNodeTree = function(currentNodeId, parentId) {
-		var element = goog.dom.getElement(currentNodeId)
-
+	ZH.initNodeTree = function(entry) {
+    var currentNodeId = entry['id']
+    var element = goog.dom.getElement(currentNodeId)
     if (!element) {
-      throw new Error('No element found for:' + currentNodeId)
+      throw new Error('Can not find element for: ' + currentNodeId + '@' + entry['js'])
+    }
+    //we got the info for this node.
+    var nodeConstructor = registry.getConstructor(entry['js'])
+    var instance_;
+    if (nodeConstructor) {
+      instance_ = nodeConstructor.createInstance?nodeConstructor.createInstance():null;
+
+      //this type is Singleton.
+      if (!instance_) {
+        instance_ = nodeConstructor.getInstance?nodeConstructor.getInstance():null;
+      }
+
+      if ( !instance_ && goog.isFunction(nodeConstructor) ) {
+        instance_ = new nodeConstructor(
+          // This trusted source.
+          goog.json.unsafeParse(element.getAttribute('data-meta') || '{}')
+          , goog.json.unsafeParse(element.getAttribute('data-options') || '{}')
+          );
+      }
+
     }
 
-    goog.array.forEach(ZH.parentChildMap[parentId], function(entry, index) {
-      if (entry['id'] === currentNodeId) {
-        //we got the info for this node.
-        var nodeConstructor = registry.getConstructor(entry['js'])
-        var instance_;
-        if (nodeConstructor) {
-          instance_ = nodeConstructor.createInstance?nodeConstructor.createInstance():null;
-
-          //this type is Singleton.
-          if (!instance_) {
-            instance_ = nodeConstructor.getInstance?nodeConstructor.getInstance():null;
-          }
-
-          if (!instance_ && goog.isFunction(nodeConstructor)) {
-            instance_ = new nodeConstructor(); 
-          }
-
+    if (instance_) {
+      instance_.decorate(element)
+      // Check if has any child node.
+      if (ZH.parentChildMap[currentNodeId]) {
+        var next = ZH.parentChildMap[currentNodeId];
+        var count = next.length;
+        for (var k = 0;k < count;k++) {
+          arguments.caller.callee(next[k])  
         }
+      } 
+    } else {
+      throw new Error('Can not create instance for type: ' + entry['js'])
+    }
 
-        if (instance_) {
-          instance_.decorate(element)
-        } else {
-          throw new Error('Can not create instance for type: ' + entry['js'])
-        }
-      }
-    })
 	}
 
   ZH.initComponent = function(dependencyTree, parentChildMap) {
   	ZH.dependencyTree = dependencyTree
 		ZH.parentChildMap = parentChildMap
-  	if (console) {
-  		console.log('init component loaded in !!!!')
-  	}
 
   	for (k in dependencyTree) {
   		if (dependencyTree.hasOwnProperty(k)){
   			var deps = dependencyTree[k];
-  			//get all deps for a tre.
+  			//get all deps for a tree.
   			var rootElementId = k;
-  			require.async(deps, goog.partial(function(rootElementId, parentId){
-  				ZH.initNodeTree(rootElementId, parentId)
-  			}, rootElementId, 'ROOT'))
+        var nodeInfo = goog.array.find(ZH.parentChildMap['ROOT'], function(entry){
+          return entry['id'] === rootElementId
+        })
+  			require.async(deps, goog.partial(function(info){
+  				ZH.initNodeTree(info)
+  			}, goog.object.clone(nodeInfo))
   		}
   	}
   }
--- src/net_pagelet.js
+++ src/net_pagelet.js
@@ -1,36 +1,37 @@
 goog.provide('ZH.net.Pagelet')
 
+// Pagelet wrapper class.
+// A pagelet represent a single tree node.
 ZH.net.Pagelet = function(opt_pageletObject) {
-  // Module identity? for seaJS to require.
-  this.module = opt_pageletObject['module']
-  // Client id.
-  this.id = opt_pageletObject['id']
   this.html = opt_pageletObject['html']
+  
+  //{'id' : xxx, js:xxx, css: xx}
+  this.rootNode = opt_pageletObject['root_node']
+
   // Keep parents/child mapping.
-  this.map = opt_pageletObject['map']
-  this.referElement = opt_pageletObject['reffer_node']
-  this.renderType = opt_pageletObject['render_type']
-  this.renderPosition = opt_pageletObject['render_position']
-  this.eventCategory = opt_pageletObject['event_category']
-  this.eventArgs = opt_pageletObject['event_args']
+  this.inforMap = opt_pageletObject['infor_map']
+
+  this.referElement = opt_pageletObject['message']['reffer_node'],
+  this.renderType = opt_pageletObject['message']['render_type'],
+  this.renderPosition = opt_pageletObject['message']['render_position'],
+
+  // Optional, this is just for update single node.
+  if (opt_pageletObject['message']) {
+    this.message = {
+      eventName : opt_pageletObject['message']['event_name'],
+      eventArgs : opt_pageletObject['message']['event_args']
+    }  
+  }
+  
 }
 
-// ZH.net.Pagelet.prototype.typeString = null
-ZH.net.Pagelet.prototype.module = null
+ZH.net.Pagelet.prototype.rootNodeList = null
 
-ZH.net.Pagelet.prototype.id = null
+ZH.net.Pagelet.prototype.inforMap = null
 
-ZH.net.Pagelet.prototype.html = null
+ZH.net.Pagelet.prototype.html = ''
 
-ZH.net.Pagelet.prototype.referElement = null
-
-ZH.net.Pagelet.prototype.renderType = null
-
-ZH.net.Pagelet.prototype.renderPosition = null
-
-ZH.net.Pagelet.prototype.eventType = null
-
-ZH.net.Pagelet.prototype.eventArgs = null
+ZH.net.Pagelet.prototype.message = {}
 
 
 
@@ -44,7 +45,7 @@ ZH.net.Pagelet.RenderPosition = {
 ZH.net.Pagelet.RenderType = {
   RENDER: 'render',
   DECORATION: 'decoration',
-  NONE: 'none',// used for none UI component.
+  NONE: 'none',// used for existing component.
   UPDATING: 'updating', //used for live node update.
   UN_RENDER: 'un_render' //remove component.
 };
